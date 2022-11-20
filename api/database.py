a5aff5afc1d18d6663d420838e7181bed0e90e96
AoWangPhilly <aw3338@drexel.edu>
2022-11-20 17:01:31 -0500

    test

--- api/database.py
+++ api/database.py
@@ -2,12 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import database_exists, create_database
-from decouple import config
+from .config import settings
 
-SQLALCHEMY_DATABASE_URL = "postgresql://tqpmkcmcriksac:10a8ebb5db620acbacda22cc96582fd74d147eef705cbf641ee155847085f9df@ec2-3-209-39-2.compute-1.amazonaws.com:5432/d42tgjq61pdcvl"
-    # f"postgresql://{config('USERNAME')}:{config('PASSWORD')}@{config('HOST')}:{config('PORT')}/{config('DB_NAME')}"
-
-
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
